e2cf8ad7a6481ba512744e1c4375a8ebb94bf80a
Kai Lyons <kai@potabi.com>
2021-12-12 20:39:05 -0700

    Created pc-sysinstall package for installation

---
+++ potabi/pc-sysinstall/work/.license-catalog.mk
@@ -0,0 +1,5 @@
+_LICENSE=BSD2CLAUSE
+_LICENSE_NAME=BSD 2-clause Simplified License
+_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
+_LICENSE_GROUPS=FSF OSI COPYFREE
+_LICENSE_DISTFILES=ghostbsd-pc-sysinstall-2021041900-acf83bcb77d275c8cf7c56fc59d127409d4a4587_GH0.tar.gz
